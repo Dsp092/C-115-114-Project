63a50a82d25b65e3125bda05597d81918a0e3ebc
Dsp092 <78990925+Dsp092@users.noreply.github.com>
2022-06-30 12:59:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload()
+{
+
+}
+
+
+function setup()
+{
+canvas=createCanvas(300,300);
+canvas.center();
+video= createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+
+function gotPoses(results)
+{
+if (results.length>0) 
+{
+    console.log(results);
+    nosex=results[0].pose.nose.x;
+    nosey=results[0].pose.nose.y;
+    console.log(nosex);
+    console.log(nosey);
+}
+}
+
+
+function modelLoaded()
+{
+console.log("poseNetisinsitilatized");
+}
+
+
+function draw()
+{
+image(video,0,0,300,300);
+}
+
+
+function takesnapshot()
+{
+    save('class123.png');
+}
+
